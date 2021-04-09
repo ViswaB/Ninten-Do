6d4ea7e29aea6baba791104256da10d894447464
Alex Bush <qdf438@my.utsa.edu>
2021-04-08 21:41:28 -0500

    files for stats scene

---
+++ statsScene/StatsController.java
@@ -0,0 +1,44 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+
+public class StatsController {
+
+    @FXML
+    private Label CurrentTasks;
+
+    @FXML
+    private Label UserStats;
+
+    
+
+    @FXML
+    private Label Points;
+
+    
+
+    @FXML
+    private ProgressBar p3;
+
+    @FXML
+    private Label WeeklyTasks;
+
+    @FXML
+    private Label Level;
+
+    @FXML
+    private Label MonthlyTasks;
+
+    @FXML
+    private Button Home;
+    
+    
+    //p1 = new ProgressBar(0.5);
+    
+    
+
+}
