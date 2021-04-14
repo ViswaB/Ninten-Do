295b73da5c630855ef895431410b5d0aa1912804
Filiberto Rios <filibertoerios@gmail.com>
2021-04-14 00:56:36 -0500

    maybe finished userdata singleton? not too sure will check later, fixed images for login and register screen, finished user model class

--- src/model/UserData.java
+++ src/model/UserData.java
@@ -1,22 +1,80 @@
 package model;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.EOFException;
+import java.io.InvalidClassException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class UserData {
 	private static UserData instance = new UserData();
-	private static String usersFile;
-	private HashMap<Integer, User> allUsers;
+	private static String usersFile = "users.dat";
+	private User loggedInUser;
+	private ArrayList<User> allUsers;
 	private Path filePath;
-	
+
 	private UserData() {}
 	
-	public User getUser(int uid) {
-		return this.allUsers.get(uid);
+	public boolean getUser(String userName, String password) {
+		for(User user : allUsers) {
+			if(user.getUserName().equals(userName) && user.getPassword().equals(password)) {
+				this.loggedInUser = user;
+				return true;
+			}
+		}
+		return false;
 	}
 	
-	public void addUser(User user, int uid) {
-		this.allUsers.put(uid, user);
+	public User retrieveUser() {
+		return this.loggedInUser;
+	}
+	
+	public void  addUser(User user) {
+		this.allUsers.add(user);
+	}
+	
+	public void loadUsers() {
+		allUsers = new ArrayList<User>();
+		filePath = FileSystems.getDefault().getPath(usersFile);
+		
+		try(ObjectInputStream uFile = new ObjectInputStream(new BufferedInputStream(Files.newInputStream(filePath)))){
+			boolean eof = false;
+			while(!eof) {
+				try {
+					User user = (User) uFile.readObject();
+					allUsers.add(user);
+				}catch(EOFException e) {
+					eof = true;
+				}
+			}
+		}catch(InvalidClassException e) {
+			e.printStackTrace();
+		}catch(IOException e) {
+			e.printStackTrace();
+		}catch(ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void saveUsers() {
+		filePath = FileSystems.getDefault().getPath(usersFile);
+		
+		try(ObjectOutputStream uFile = new ObjectOutputStream(new BufferedOutputStream(Files.newOutputStream(filePath)))){
+			Iterator<User> iter = allUsers.iterator();
+			while(iter.hasNext()) {
+				User user = iter.next();
+				uFile.writeObject(user);
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public UserData getInstance() {
